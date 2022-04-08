77b7a1dc5fc98d5a4eba6e51d542597aa5cb0865
jake <jakebs540@gmail.com>
2022-04-08 14:40:59 +0100

    View or Post advert page created

--- app/routes.js
+++ app/routes.js
@@ -2,5 +2,15 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+// Run this code when a form is submitted to 'juggling-balls-answer'
+router.post('/view-post-answer', function (req, res) {
+  var viewPostAnswer = req.session.data['view-or-post-advert']
+
+  if (viewPostAnswer == "veiw-advert"){
+    res.redirect('/view-advert-region')
+  } else {
+    res.redirect('/post-advert-region')
+  }
+})
 
 module.exports = router
